382ab5e34222eba1ba7566d42ac7f0101f618d40
StacyDu <dudinaanastasiia@gmail.com>
2017-12-18 22:07:15 +0100

    Added the js code for the third section

--- js/DuBlog.js
+++ js/DuBlog.js
@@ -1,2 +1,40 @@
+document.addEventListener("DOMContentLoaded", function (){
+    var photos = {
+        'Barcelona': "text for barcelona picture",
+        'Venice': "text for venice picture",
+        'New Year': "text for christmas picture",
+    };
+    var carousel = document.getElementById("mainCarousel");
+    var changedText = document.getElementById("changedText");
+    var rightCntrl = document.getElementsByClassName("right")[0];
+    var leftCntrl = document.getElementsByClassName("left")[0];
 
+
+    // var myEventFunc = function(pictureDiv){
+    //     var keyWord = pictureDiv.getElementsByClassName('active')[0].firstElementChild.getAttribute("alt");
+    //     changedText.innerText = photos[keyWord];
+    // };
+
+    // rightCntrl.addEventListener("click", myEventFunc(carousel));
+
+
+
+    rightCntrl.addEventListener("click", function(){
+        var keyWord = carousel.getElementsByClassName('active')[0].firstElementChild.getAttribute("alt");
+        changedText.innerText = photos[keyWord];
+        console.log(changedText.innerText);
+        console.log(keyWord);
+        
+    });
+
+
+    leftCntrl.addEventListener("click", function(){
+        var keyWord = carousel.getElementsByClassName('active')[0].firstElementChild.getAttribute("alt");
+        changedText.innerText = photos[keyWord];
+        console.log(changedText.innerText);
+        console.log(keyWord);
+        
+    });
+
+});
 
