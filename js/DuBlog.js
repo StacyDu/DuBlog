cc49d1e3648b517af4e3e1ed80d3b7d757de4c99
StacyDu <dudinaanastasiia@gmail.com>
2017-12-26 17:23:15 +0100

    The js code for the section 'travel' done

--- js/DuBlog.js
+++ js/DuBlog.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function (){
     var photos = {
         'Barcelona': "text for barcelona picture",
@@ -9,32 +10,63 @@ document.addEventListener("DOMContentLoaded", function (){
     var rightCntrl = document.getElementsByClassName("right")[0];
     var leftCntrl = document.getElementsByClassName("left")[0];
 
-
-    // var myEventFunc = function(pictureDiv){
-    //     var keyWord = pictureDiv.getElementsByClassName('active')[0].firstElementChild.getAttribute("alt");
-    //     changedText.innerText = photos[keyWord];
-    // };
-
-    // rightCntrl.addEventListener("click", myEventFunc(carousel));
-
-
+    var counter = 0;
+    changedText.innerText = photos["Barcelona"];
 
     rightCntrl.addEventListener("click", function(){
-        var keyWord = carousel.getElementsByClassName('active')[0].firstElementChild.getAttribute("alt");
-        changedText.innerText = photos[keyWord];
-        console.log(changedText.innerText);
-        console.log(keyWord);
-        
-    });
+        counter++;
+        if (counter === 0){
+            changedText.innerText = photos["Barcelona"];
+        }else if(counter ===1){
+            changedText.innerText = photos["Venice"];               
+        }else if(counter === 2){
+            changedText.innerText = photos["New Year"];
+        }else if(counter>2){
+            counter = 0;
+            changedText.innerText = photos["Barcelona"];
+        }     
+   });
+
+   leftCntrl.addEventListener("click", function(){
+    counter--;
+    if (counter === 0){
+        changedText.innerText = photos["Barcelona"];
+    }else if(counter === 1){
+        changedText.innerText = photos["Venice"];               
+    }else if(counter === 2){
+        changedText.innerText = photos["New Year"];
+    }else if(counter<0){
+        counter = 2;
+        changedText.innerText = photos["New Year"];
+    }     
+});
 
 
-    leftCntrl.addEventListener("click", function(){
-        var keyWord = carousel.getElementsByClassName('active')[0].firstElementChild.getAttribute("alt");
-        changedText.innerText = photos[keyWord];
-        console.log(changedText.innerText);
-        console.log(keyWord);
-        
-    });
+//     leftCntrl.addEventListener("click", function(){
+//         var keyWord = carousel.getElementsByClassName('active')[0].firstElementChild.getAttribute("alt");
+//         changedText.innerText = photos[keyWord];
+//         });
+
+
+    //     rightCntrl.addEventListener("click", function(){
+    //         var allChildren = carousel.children;
+    //         for(i = 0; i <allChildren.length; i++){
+    //             if  (allChildren[i].classList.contains("active")){
+    //                 changedText.innerText = photos[allChildren[i+1].firstElementChild.getAttribute("alt")];
+    //             }
+    //         };
+
+    //    });
+
+    //    leftCntrl.addEventListener("click", function(){
+    //     var allChildren = carousel.children;
+    //     for(i = 0; i <allChildren.length; i++){
+    //         if  (allChildren[i].classList.contains("active")){
+    //             changedText.innerText = photos[allChildren[i-1].firstElementChild.getAttribute("alt")];
+    //         }
+    //     };
+
+    //     });
 
 });
 
